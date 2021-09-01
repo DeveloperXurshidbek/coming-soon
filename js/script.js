bd3ac1cff455e0fd336e0d1c131d60161dc8a280
Xurshidbek <74502182+DeveloperXurshidbek@users.noreply.github.com>
2021-09-01 23:18:08 +0500

    Updating

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let countDate = new Date('aug 27, 2021 00:00:00').getTime();
+let countDate = new Date('sep 30, 2021 00:00:00').getTime();
 
 function countDown() {
     let now = new Date().getTime();
