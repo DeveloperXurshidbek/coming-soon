4a51802c19fac3667fca15608a0666dfd6e1d818
Xurshidbek <74502182+DeveloperXurshidbek@users.noreply.github.com>
2023-03-14 05:58:02 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-window.onload = function() {
-    window.location.href = "www.instagram.com/"
-
-}
-
-/* let countDate = new Date('may 30, 2023 00:00:00').getTime();
+let countDate = new Date('may 30, 2023 00:00:00').getTime();
 
 function countDown() {
     let now = new Date().getTime();
@@ -33,5 +28,3 @@ window.replainSettings = { id: '20b7d890-825c-46fd-a349-7769f441fb2b' };
 (function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;
 var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
 })('https://widget.replain.cc/dist/client.js');
-
-*/
